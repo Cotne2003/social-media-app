30f346650d803ce7fe8b133a13d87b2b620c57cc
Cotne Basiashvili <basiashvilic@gmail.com>
2024-06-10 21:10:30 +0400

    added player card and house card

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,20 +1,36 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Image from "next/image";
 import Rules from "./Rules";
 import data from "@/data";
 
+type DataProp = {
+  img: string;
+  id: number;
+  name: string;
+};
+
 const HomePage = () => {
-  const [playerCard, setPlayerCard] = useState<undefined | number>(undefined);
-  console.log(playerCard);
+  const [playerCard, setPlayerCard] = useState<undefined | DataProp>(undefined);
+  const [houseCard, setHouseCard] = useState<undefined | DataProp>(undefined);
+
+  useEffect(() => {
+    if (playerCard !== undefined) {
+      setTimeout(() => {
+        const filteredData = data.filter((card) => card.id !== playerCard?.id);
+        const randomNumber = +Math.random().toFixed();
+        setHouseCard(filteredData[randomNumber]);
+      }, 1000);
+    }
+  }, [playerCard]);
 
   return (
-    <main className="pt-10 flex flex-col h-screen">
+    <main className="pt-10 flex flex-col h-screen items-center">
       <Header />
-      {playerCard === undefined && (
-        <section className="flex flex-col items-center justify-evenly h-full">
+      {playerCard === undefined ? (
+        <section className="mt-[60px] h-full">
           <div className="flex flex-col items-center gap-5">
             <div className="flex gap-10">
               <Image
@@ -22,14 +38,18 @@ const HomePage = () => {
                 width={125}
                 height={125}
                 alt="hand"
-                onClick={() => setPlayerCard(data[0].id)}
+                onClick={() => {
+                  setPlayerCard(data[0]);
+                }}
               />
               <Image
                 src={data[1].img}
                 width={125}
                 height={125}
                 alt="scissor"
-                onClick={() => setPlayerCard(data[1].id)}
+                onClick={() => {
+                  setPlayerCard(data[1]);
+                }}
               />
             </div>
             <div>
@@ -38,14 +58,44 @@ const HomePage = () => {
                 width={125}
                 height={125}
                 alt="fist"
-                onClick={() => setPlayerCard(data[2].id)}
+                onClick={() => {
+                  setPlayerCard(data[2]);
+                }}
               />
+            </div>
+          </div>
+        </section>
+      ) : (
+        <section className="mt-[60px] h-full w-[350px]">
+          <div className="flex justify-between items-center">
+            <div className="flex flex-col items-center gap-5">
+              <Image
+                src={playerCard.img}
+                width={125}
+                height={125}
+                alt="selected"
+              />
+              <h2 className="font-semibold">YOU PICKED</h2>
+            </div>
+            <div className="flex flex-col items-center gap-5">
+              {houseCard ? (
+                <Image
+                  src={houseCard ? houseCard.img : ""}
+                  width={125}
+                  height={125}
+                  alt="asd"
+                />
+              ) : (
+                <div className="w-[125px] h-[125px] rounded-[50%] bg-slate-800"></div>
+              )}
+
+              <h2 className="font-semibold">THE HOUSE PICKED</h2>
             </div>
           </div>
         </section>
       )}
 
-      <div className="flex justify-center mb-10">
+      <div className="w-[130px] absolute bottom-10">
         <Rules />
       </div>
     </main>
